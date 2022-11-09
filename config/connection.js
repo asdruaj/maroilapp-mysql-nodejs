4b70667ffe4605812c507c5801cbc06e4fda1880
Asdrubal Aldana <asdrubal.aldana12@gmail.com>
2022-11-09 12:52:54 -0400

    recent updates fix

--- config/connection.js
+++ config/connection.js
@@ -33,7 +33,7 @@ const program = async () => {
 
   instance.addTrigger({
     name: 'TEST',
-    expression: `railway.tbl-reports`,
+    expression: 'railway.tbl-reports',
     statement: MySQLEvents.STATEMENTS.ALL,
     onEvent: async (event) => { // You will receive the events here
       let updatedObject = {
